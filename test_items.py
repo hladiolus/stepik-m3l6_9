b21a28dbb751d847e1fe8b765afe935cdc1675e1
hladiolus <zaporozhec.bogdan@gmail.com>
2021-04-21 13:30:30 +0300

    solution https://stepik.org/lesson/237240/step/9

---
+++ test_items.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def is_element_present(browser, by, text):
+    try:
+        return WebDriverWait(browser, 12).until(EC.presence_of_element_located((by, text)))
+    except TimeoutException:
+        return False
+
+
+def test_add_to_cart_present(browser):
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    assert is_element_present(browser, By.CSS_SELECTOR, ".btn-add-to-basket"), "Button for adding item to the cart was not found."
