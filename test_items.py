22c806912006ca6cd6291e4c21b91edc53d1b746
hladiolus <zaporozhec.bogdan@gmail.com>
2021-04-21 13:38:52 +0300

    updated

--- test_items.py
+++ test_items.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import time
 
 def is_element_present(browser, by, text):
     try:
